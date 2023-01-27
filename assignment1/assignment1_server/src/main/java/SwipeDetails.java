d65b37327982a0d3851266657ff558c2fa9a5cef
kejiantong <tong.ke@northeastern.edu>
2023-01-26 23:19:00 -0800

    update & revise server code

--- assignment1/assignment1_server/src/main/java/SwipeDetails.java
+++ assignment1/assignment1_server/src/main/java/SwipeDetails.java
@@ -8,23 +8,12 @@ public class SwipeDetails {
     return swiper;
   }
 
-  public void setSwiper(String swiper) {
-    this.swiper = swiper;
-  }
-
   public String getSwipee() {
     return swipee;
-  }
-
-  public void setSwipee(String swipee) {
-    this.swipee = swipee;
   }
 
   public String getComment() {
     return comment;
   }
 
-  public void setComment(String comment) {
-    this.comment = comment;
-  }
 }
--- assignment1/assignment1_server/src/main/java/SwipeServlet.java
+++ assignment1/assignment1_server/src/main/java/SwipeServlet.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 
 @WebServlet(name = "SwipeServlet", value = "/SwipeServlet")
 public class SwipeServlet extends HttpServlet {
-//  private ServletsUtil servletsUtil = new ServletsUtil();
-
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
@@ -41,17 +39,6 @@ public class SwipeServlet extends HttpServlet {
       return;
     }
 
-    // check if request json body is valid
-//    String jsonBodyString = servletsUtil.getJsonBodyString(req);
-//    if(! isJsonBodyValid(jsonBodyString, res)) {
-//      responseMsg.setMessage("Invalid json body");
-//      res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//      res.getOutputStream().print(gson.toJson(responseMsg));
-//      res.getOutputStream().flush();
-////      res.getWriter().write("Invalid json body");
-//      return;
-//    }
-
     try {
       StringBuilder sb = new StringBuilder();
       String s;
@@ -81,69 +68,10 @@ public class SwipeServlet extends HttpServlet {
       res.getOutputStream().print(gson.toJson(responseMsg));
       res.getOutputStream().flush();
     }
-
-//    if (method == HttpMethod.POST) {
-//      res.setStatus(HttpServletResponse.SC_CREATED);
-//      JsonObject jsonBody = new JsonParser().parse(jsonBodyString).getAsJsonObject();
-//      int swiper = jsonBody.get("swiper").getAsInt();
-//      int swipee = jsonBody.get("swipee").getAsInt();
-//      String comment = jsonBody.get("comment").getAsString();
-//      String resBody = "Write successful";
-//
-//      JsonObject jsonObject = new JsonObject();
-//      servletsUtil.writeJsonObject(res, resBody, jsonObject);
-//    }
   }
 
-//  private boolean isJsonBodyValid(String jsonBodyString, HttpServletResponse res)
-//      throws IOException {
-//    try {
-//      JsonObject jsonBody = new JsonParser().parse(jsonBodyString).getAsJsonObject();
-//      // check if swiper is a number between 1 and 5000
-//      String swipers = jsonBody.get("swiper").getAsString();
-//
-//      if(!isValidNumber(swipers)) {
-//        res.getWriter().write("Not valid number\n");
-//        return false;
-//      }
-//      int swiper = Integer.parseInt(swipers);
-//      if (swiper < 1 || swiper > 5000) {
-//        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//        res.getWriter().write("Invalid swiper value. Must be a number between 1 and 5000.");
-//        return false;
-//      }
-//
-//      // check if swipee is a number between 1 and 1000000
-//      String swipees = jsonBody.get("swipee").getAsString();
-//      if(!isValidNumber(swipees)) {
-//        res.getWriter().write("Not a valid number\n");
-//        return false;
-//      }
-//      int swipee = Integer.parseInt(swipees);
-//      if (swipee < 1 || swipee > 1000000) {
-//        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//        res.getWriter().write("Invalid swipee value. Must be a number between 1 and 1000000.");
-//        return false;
-//      }
-//
-//      // check if comment is a string of 256 characters or less
-//      String comment = jsonBody.get("comment").getAsString();
-//      if (comment.length() > 256) {
-//        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//        res.getWriter().write("Invalid comment value. Must be a string of 256 characters or less.");
-//        return false;
-//      }
-//
-//    } catch (JsonSyntaxException e) {
-//      res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//      res.getWriter().write("Error parsing json body.");
-//      return false;
-//    }
-//    return true;
-//  }
-
   private boolean isValidUrl(String[] urlParts) {
-    if (urlParts[1].equals("left") || urlParts[1].equals("right") ) {
+    if ((urlParts[1].equals("left") && urlParts.length == 2) || (urlParts[1].equals("right") && urlParts.length == 2)) {
       return true;
     }
     return false;
