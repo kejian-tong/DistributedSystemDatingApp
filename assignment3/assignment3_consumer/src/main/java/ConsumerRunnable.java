fb6bab90c18cf2092a1ef4277e0b3b041bf8c78d
kejiantong <tong.ke@northeastern.edu>
2023-03-27 03:38:41 -0700

    change write bacth to tread-safe

--- assignment3/assignment3_consumer/src/main/java/ConsumerRunnable.java
+++ assignment3/assignment3_consumer/src/main/java/ConsumerRunnable.java
@@ -3,6 +3,7 @@ import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.InsertOneModel;
+import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.WriteModel;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -10,9 +11,11 @@ import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -35,6 +38,10 @@ public class ConsumerRunnable implements Runnable{
       new LinkedBlockingQueue<>()
   );
 
+  // Use ConcurrentLinkedQueue instead of ArrayList for thread safety
+  private final Queue<Document> swipeBatch = new ConcurrentLinkedQueue<>();
+//  private final List<Document> swipeBatch = new ArrayList<>();
+
   public ConsumerRunnable(Connection connection, MongoDatabase database) {
     this.connection = connection;
     this.database = database;
@@ -55,14 +62,12 @@ public class ConsumerRunnable implements Runnable{
       throw new RuntimeException(e);
     }
 
-    List<Document> swipeBatch = new ArrayList<>();
-
     // Timer to periodically flush remaining events in the batch
     Timer timer = new Timer();
     timer.schedule(new TimerTask() {
       @Override
       public void run() {
-        flushBatch(swipeBatch);
+        flushBatch((ConcurrentLinkedQueue<Document>) swipeBatch);
       }
     }, flushInterval, flushInterval);
 
@@ -84,7 +89,7 @@ public class ConsumerRunnable implements Runnable{
 
       // Check if the batch size reached 1000
       if (swipeBatch.size() >= batchSize) {
-        flushBatch(swipeBatch);
+        flushBatch((ConcurrentLinkedQueue<Document>) swipeBatch);
       }
 
       // ack the receipt and indicating the message can be removed from queue
@@ -98,9 +103,9 @@ public class ConsumerRunnable implements Runnable{
     }
   }
 
-  public synchronized void flushBatch(List<Document> swipeBatch) {
+  public void flushBatch(ConcurrentLinkedQueue<Document> swipeBatch) {
     if (!swipeBatch.isEmpty()) {
-      List<Document> batchToProcess = new ArrayList<>(swipeBatch);
+      ConcurrentLinkedQueue<Document> batchToProcess = new ConcurrentLinkedQueue<>(swipeBatch);
       swipeBatch.clear();
 
       // Submit the task to the thread pool
@@ -120,13 +125,13 @@ public class ConsumerRunnable implements Runnable{
           boolean isLike = doc.getBoolean("isLike");
 
           // Update user stats
-          MultiThreadConsumer.updateUserStats(database, swiper, isLike);
+          updateUserStats(swiper, isLike);
 
           // Check for a match and insert it if found
           if (isLike) {
             Set<Integer> swipeRightSet = SwipeRecord.listSwipeRight.get(swipee);
             if (swipeRightSet != null && swipeRightSet.contains(swiper)) {
-              MultiThreadConsumer.insertMatch(database, swiper, swipee);
+              insertMatch(swiper, swipee);
             }
             SwipeRecord.addToLikeMap(swiper, swipee, true);
           }
@@ -134,5 +139,65 @@ public class ConsumerRunnable implements Runnable{
       });
     }
   }
+
+  public void insertMatch(Integer swiper, Integer swipee) {
+    Document matchDocument = new Document("swiper", swiper)
+        .append("swipee", swipee);
+    MongoCollection<Document> collection = database.getCollection("matches");
+    collection.insertOne(matchDocument);
+  }
+
+
+  public void updateUserStats(Integer swiper, boolean isLike) {
+    MongoCollection<Document> collection = database.getCollection("stats");
+    Document query = new Document("swiper", swiper);
+    Document update;
+    if (isLike) {
+      update = new Document("$inc", new Document("numLikes", 1));
+    } else {
+      update = new Document("$inc", new Document("numDislikes", 1));
+    }
+    synchronized (this) {
+      collection.updateOne(query, update, new UpdateOptions().upsert(true));
+    }
+  }
+
+  // uncomment this method to use ArrayList instead of ConcurrentLinkedQueue
+//  public synchronized void flushBatch(List<Document> swipeBatch) {
+//    if (!swipeBatch.isEmpty()) {
+//      List<Document> batchToProcess = new ArrayList<>(swipeBatch);
+//      swipeBatch.clear();
+//
+//      // Submit the task to the thread pool
+//      executor.submit(() -> {
+//        // Perform batch write
+//        MongoCollection<Document> collection = database.getCollection("swipeData");
+//        List<WriteModel<Document>> bulkOperations = new ArrayList<>();
+//        for (Document doc : batchToProcess) {
+//          bulkOperations.add(new InsertOneModel<>(doc));
+//        }
+//        BulkWriteResult result = collection.bulkWrite(bulkOperations);
+//
+//        // Update user stats and insert matches in a loop
+//        for (Document doc : batchToProcess) {
+//          int swiper = doc.getInteger("swiper");
+//          int swipee = doc.getInteger("swipee");
+//          boolean isLike = doc.getBoolean("isLike");
+//
+//          // Update user stats
+//          MultiThreadConsumer.updateUserStats(database, swiper, isLike);
+//
+//          // Check for a match and insert it if found
+//          if (isLike) {
+//            Set<Integer> swipeRightSet = SwipeRecord.listSwipeRight.get(swipee);
+//            if (swipeRightSet != null && swipeRightSet.contains(swiper)) {
+//              MultiThreadConsumer.insertMatch(database, swiper, swipee);
+//            }
+//            SwipeRecord.addToLikeMap(swiper, swipee, true);
+//          }
+//        }
+//      });
+//    }
+//  }
 }
 
--- assignment3/assignment3_consumer/src/main/java/MultiThreadConsumer.java
+++ assignment3/assignment3_consumer/src/main/java/MultiThreadConsumer.java
@@ -1,59 +1,83 @@
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.UpdateOptions;
 import com.rabbitmq.client.Connection;
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeoutException;
 import com.rabbitmq.client.ConnectionFactory;
-import org.bson.Document;
 
 
 public class MultiThreadConsumer {
   private static Integer NUM_PER_THREADS = Constant.NUM_PER_THREADS;
+  // Create a single MongoClient instance and share it across threads
+  // each thread will have its own connection to the database
+  private static MongoClient mongoClient; // MongoDB client is thread safe and it's recommended to share one instance across threads
 
 
   public static void main (String[] args) throws IOException, TimeoutException {
-
-//    CountDownLatch numberOfThreads = new CountDownLatch(20);
     // Set up MongoDB URI
     String uri = "mongodb://admin:admin@18.237.89.233:27017/?maxPoolSize=50"; // ec2 public ip
 
     // Create MongoDB client
-    MongoClientURI mongoClientURI = new MongoClientURI(uri);
-    MongoClient mongoClient = new MongoClient(mongoClientURI);
-
-    // Get MongoDB database
-    MongoDatabase database = mongoClient.getDatabase("admin");
-    // Check if collections exist before creating them
-    if (!collectionExists(database, "matches")) {
-      database.createCollection("matches");
+    if(mongoClient == null) {
+      MongoClientURI mongoClientURI = new MongoClientURI(uri);
+      mongoClient = new MongoClient(mongoClientURI);
     }
 
-    if (!collectionExists(database, "stats")) {
-      database.createCollection("stats");
-    }
+//    // Get MongoDB database
+//    MongoDatabase database = mongoClient.getDatabase("admin");
+//    // Check if collections exist before creating them
+//    if (!collectionExists(database, "matches")) {
+//      database.createCollection("matches");
+//    }
+//
+//    if (!collectionExists(database, "stats")) {
+//      database.createCollection("stats");
+//    }
+//
+//    ConnectionFactory connectionFactory = new ConnectionFactory();
+//    connectionFactory.setHost(Constant.HOST_NAME);
+//    connectionFactory.setVirtualHost("cherry_broker"); // added ec2 RMQ vhost
+//    connectionFactory.setPort(5672);
+////    connectionFactory.setUsername("guest");
+////    connectionFactory.setPassword("guest");
+//    // for ec2 user, should be used the below one
+//    connectionFactory.setUsername("admin");
+//    connectionFactory.setPassword("admin");
+//    Connection connection = connectionFactory.newConnection();
+//    ExecutorService executorService = Executors.newFixedThreadPool(Constant.NUM_PER_THREADS);
+//
+//    for(int i = 0; i < NUM_PER_THREADS; i++) {
+//      executorService.execute(new ConsumerRunnable(connection, database));
+//    }
 
-    ConnectionFactory connectionFactory = new ConnectionFactory();
-    connectionFactory.setHost(Constant.HOST_NAME);
-    connectionFactory.setVirtualHost("cherry_broker"); // added ec2 RMQ vhost
-    connectionFactory.setPort(5672);
-//    connectionFactory.setUsername("guest");
-//    connectionFactory.setPassword("guest");
-    // for ec2 user, should be used the below one
-    connectionFactory.setUsername("admin");
-    connectionFactory.setPassword("admin");
-    Connection connection = connectionFactory.newConnection();
+    // Get MongoDB database for each thread
     ExecutorService executorService = Executors.newFixedThreadPool(Constant.NUM_PER_THREADS);
 
     for(int i = 0; i < NUM_PER_THREADS; i++) {
+      MongoDatabase database = mongoClient.getDatabase("admin");
+
+      // Check if collections exist before creating them
+      if (!collectionExists(database, "matches")) {
+        database.createCollection("matches");
+      }
+
+      if (!collectionExists(database, "stats")) {
+        database.createCollection("stats");
+      }
+
+      ConnectionFactory connectionFactory = new ConnectionFactory();
+      connectionFactory.setHost(Constant.HOST_NAME);
+      connectionFactory.setVirtualHost("cherry_broker"); // added ec2 RMQ vhost
+      connectionFactory.setPort(5672);
+      connectionFactory.setUsername("admin");
+      connectionFactory.setPassword("admin");
+      Connection connection = connectionFactory.newConnection();
+
       executorService.execute(new ConsumerRunnable(connection, database));
     }
-
 
     // Close the connection
     while (!executorService.isShutdown()) {
@@ -71,23 +95,24 @@ public class MultiThreadConsumer {
     return false;
   }
 
-  public static void insertMatch(MongoDatabase database, Integer swiper, Integer swipee) {
-    Document matchDocument = new Document("swiper", swiper)
-        .append("swipee", swipee);
-    MongoCollection<Document> collection = database.getCollection("matches");
-    collection.insertOne(matchDocument);
-  }
+  // commented for helper which is called in ConsumerRunnable.flushBatch()
+//  public static void insertMatch(MongoDatabase database, Integer swiper, Integer swipee) {
+//    Document matchDocument = new Document("swiper", swiper)
+//        .append("swipee", swipee);
+//    MongoCollection<Document> collection = database.getCollection("matches");
+//    collection.insertOne(matchDocument);
+//  }
 
-  public static void updateUserStats(MongoDatabase database, Integer swiper, boolean isLike) {
-    MongoCollection<Document> collection = database.getCollection("stats");
-    Document query = new Document("swiper", swiper);
-    Document update;
-    if (isLike) {
-      update = new Document("$inc", new Document("numLikes", 1));
-    } else {
-      update = new Document("$inc", new Document("numDislikes", 1));
-    }
-    collection.updateOne(query, update, new UpdateOptions().upsert(true));
-  }
+//  public static void updateUserStats(MongoDatabase database, Integer swiper, boolean isLike) {
+//    MongoCollection<Document> collection = database.getCollection("stats");
+//    Document query = new Document("swiper", swiper);
+//    Document update;
+//    if (isLike) {
+//      update = new Document("$inc", new Document("numLikes", 1));
+//    } else {
+//      update = new Document("$inc", new Document("numDislikes", 1));
+//    }
+//    collection.updateOne(query, update, new UpdateOptions().upsert(true));
+//  }
 
 }
